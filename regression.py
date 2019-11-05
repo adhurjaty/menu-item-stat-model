8389f2b0657527478a6626ce109fb72f97e4069e
Anil Dhurjaty <adhurjaty@gmail.com>
2019-11-04 17:22:56 -0800

    cleanup

--- regression.py
+++ regression.py
@@ -56,44 +56,7 @@ if __name__ == '__main__':
             sess.run(optimizer, feed_dict=feed_dict)
             print(cost.eval(feed_dict))
 
-            # if (epoch + 1) % 50 == 0: 
-            #     c = sess.run(cost, feed_dict = {xs: xi, ys: yi}) 
-            #     print("Epoch", (epoch + 1), ": cost =", c, "W =", sess.run(W), "b =", sess.run(b)) 
-      
-        # Storing necessary values to be used outside the Session 
-        # training_cost = sess.run(cost, feed_dict ={xs: x, ys: y}) 
         weight = sess.run(W) 
+        print(weight)
 
-
-    print(train_test)
-
-
-    # with tf.Session() as sess:
-    # # Initiate session and initialize all vaiables
-    # sess.run(tf.global_variables_initializer())
-    # saver = tf.train.Saver()
-    # #saver.restore(sess,'yahoo_dataset.ckpt')
-    # for i in range(100):
-    #     for j in range(X_train.shape[0]):
-    #         sess.run([cost,train],feed_dict=    {xs:X_train[j,:].reshape(1,3), ys:y_train[j]})
-    #         # Run cost and train with each sample
-    #     c_t.append(sess.run(cost, feed_dict={xs:X_train,ys:y_train}))
-    #     c_test.append(sess.run(cost, feed_dict={xs:X_test,ys:y_test}))
-    #     print('Epoch :',i,'Cost :',c_t[i])
-    # pred = sess.run(output, feed_dict={xs:X_test})
-    # # predict output of test data after training
-    # print('Cost :',sess.run(cost, feed_dict={xs:X_test,ys:y_test}))
-    # y_test = denormalize(df_test,y_test)
-    # pred = denormalize(df_test,pred)
-    # #Denormalize data     
-    # plt.plot(range(y_test.shape[0]),y_test,label="Original Data")
-    # plt.plot(range(y_test.shape[0]),pred,label="Predicted Data")
-    # plt.legend(loc='best')
-    # plt.ylabel('Stock Value')
-    # plt.xlabel('Days')
-    # plt.title('Stock Market Nifty')
-    # plt.show()
-    # if input('Save model ? [Y/N]') == 'Y':
-    #     saver.save(sess,'yahoo_dataset.ckpt')
-    #     print('Model Saved')
 
